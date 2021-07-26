a9c099c982812df68520e942248bdb2cf7b81e1d
renantescaro <renantescaro@yahoo.com.br>
2021-07-26 13:17:44 -0300

    indentação

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 450 },
+            gravity: { y: 500 },
             debug: false
         }
     },
@@ -22,8 +22,8 @@ var game
 var map
 var player
 var stars
-var corote
-var biblias
+var saida
+var inimigos
 var platforms
 var chao
 var cursors
@@ -35,26 +35,28 @@ var btnAndarEsquerda
 var btnAndarDireita
 
 function preload(){
-    this.load.image('sky', 'assets/fundo.jpg')
-    this.load.image('biblia', 'assets/biblia.png')
-    this.load.image('ground', 'assets/plataforma.png')
-    this.load.image('groundPequeno', 'assets/platformPequenaa.png')
-    this.load.image('chao', 'assets/chao.png')
-    this.load.image('star', 'assets/star.png')
-    this.load.image('corote', 'assets/corote.png')
-    this.load.image('bomb', 'assets/bomb.png')
-    this.load.spritesheet('dude', 'assets/imagem.png', { frameWidth: 98, frameHeight: 140 })
+    this.load.image('fundo',             'assets/fundo.jpg')
+    this.load.image('inimigo',           'assets/inimigo.png')
+    this.load.image('chao',              'assets/chao.png')
+    this.load.image('star',              'assets/star.png')
+    this.load.image('saida',             'assets/saida.png')
+    this.load.image('bomb',              'assets/bomb.png')
+    this.load.image('plataforma',        'assets/plataforma.png')
+    this.load.image('plataformaPequena', 'assets/platformPequenaa.png')
+
+    // personagem principal
+    this.load.spritesheet('personagem', 'assets/personagem.png', { frameWidth: 98, frameHeight: 140 })
 }
 
 function create(){
     // *** Cenário ***
 
     // background
-    this.add.image(400,  330, 'sky')
-    this.add.image(1600, 300, 'sky')
-    this.add.image(2800, 330, 'sky')
-    this.add.image(4000, 330, 'sky')
-    this.add.image(5200, 330, 'sky')
+    this.add.image(400,  330, 'fundo')
+    this.add.image(1600, 300, 'fundo')
+    this.add.image(2800, 330, 'fundo')
+    this.add.image(4000, 330, 'fundo')
+    this.add.image(5200, 330, 'fundo')
 
     platforms = this.physics.add.staticGroup()
     chao = this.physics.add.staticGroup()
@@ -66,19 +68,19 @@ function create(){
     platforms.create(3500, 670, 'chao').setScale(1).refreshBody()
 
     // plataformas
-    platforms.create(600,  545, 'ground')
-    platforms.create(1000, 360, 'ground')
-    platforms.create(1400, 300, 'ground')
-    platforms.create(2050, 400, 'ground')
-    platforms.create(2450, 400, 'groundPequeno')
-    platforms.create(2650, 400, 'groundPequeno')
-    platforms.create(2850, 400, 'groundPequeno')
-    platforms.create(3050, 450, 'groundPequeno')
-    platforms.create(3180, 230, 'groundPequeno')
-    platforms.create(3580, 230, 'groundPequeno')
+    platforms.create(600,  545, 'plataforma')
+    platforms.create(1000, 360, 'plataforma')
+    platforms.create(1400, 300, 'plataforma')
+    platforms.create(2050, 400, 'plataforma')
+    platforms.create(2450, 400, 'plataforma')
+    platforms.create(2650, 400, 'plataforma')
+    platforms.create(2850, 400, 'plataforma')
+    platforms.create(3050, 450, 'plataforma')
+    platforms.create(3180, 230, 'plataforma')
+    platforms.create(3580, 230, 'plataforma')
 
-    corote = this.physics.add.sprite(3580, 170,'corote')
-    player = this.physics.add.sprite(100, 450, 'dude')
+    saida  = this.physics.add.sprite(3580, 170,'saida')
+    player = this.physics.add.sprite(100, 450, 'personagem')
 
     // camera
     this.cameras.main.setBounds(0,0,1920*2, 700)
@@ -93,21 +95,21 @@ function create(){
     // Animações
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: -1, end: 3 }),
+        frames: this.anims.generateFrameNumbers('personagem', { start: -1, end: 3 }),
         frameRate: 10,
         repeat: -1
     })
     
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frames: this.anims.generateFrameNumbers('personagem', { start: 5, end: 8 }),
         frameRate: 10,
         repeat: -1
     })
 
     this.anims.create({
         key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
+        frames: [ { key: 'personagem', frame: 4 } ],
         frameRate: 20
     })
 
@@ -124,19 +126,19 @@ function create(){
         setXY: { x: 1000, y: 0, stepX: 205 }
     })
     
-    biblias = this.physics.add.group();
+    inimigos = this.physics.add.group();
 
    scoreText = this.add.text(100, 100, 'Pontos: 0  Vida: 100', { fontSize: '32px', fill: '#000' });
 
    // adiciona colisão entre as inimigos e as plataformas
    this.physics.add.collider(stars, platforms);
-   this.physics.add.collider(biblias, platforms);
-   this.physics.add.collider(corote, platforms);
+   this.physics.add.collider(inimigos, platforms);
+   this.physics.add.collider(saida, platforms);
    
    // quando player sobrepor uma estrela, chama o metodo collectStar
    this.physics.add.overlap(player, stars, collectStar, null, this);
-   this.physics.add.overlap(player, biblias, encostarBiblia, null, this)
-   this.physics.add.overlap(player, corote, fimFase, null, this);
+   this.physics.add.overlap(player, inimigos, encostarInimigo, null, this)
+   this.physics.add.overlap(player, saida, fimFase, null, this);
 }
 
 function update(){
@@ -199,13 +201,13 @@ function collectStar (player, star){
     score += 10;
     scoreText.setText('Pontos: ' + score + ' Vida: ' + vida);
 
-    var biblia = biblias.create(1, 1, 'biblia');
-    biblia.setBounce(1);
-    biblia.setCollideWorldBounds(true);
-    biblia.setVelocity(Phaser.Math.Between(-50, 500), 20);
+    var inimigo = inimigos.create(1, 1, 'inimigo');
+    inimigo.setBounce(1);
+    inimigo.setCollideWorldBounds(true);
+    inimigo.setVelocity(Phaser.Math.Between(-50, 500), 20);
 }
 
-function encostarBiblia(player, biblia){
+function encostarInimigo(player, inimigo){
     vida -= 1;
     scoreText.setText('Pontos: ' + score + ' Vida: ' + vida);
 
@@ -214,7 +216,7 @@ function encostarBiblia(player, biblia){
     }
 }
 
-function fimFase(player, corote){
+function fimFase(player, saida){
     document.body.style.backgroundColor = '#090'
     document.getElementsByTagName('canvas')[0].style.display = 'none'
     document.getElementById('dvFimFase').style.display = ''
